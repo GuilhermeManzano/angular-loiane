928962f32d1279b1b1970984e4190b6c13dcd491
Guilherme <guilherme.manzano93@gmail.com>
2023-04-26 10:27:03 -0300

    finish save course

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,27 +1,37 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CoursesService } from '../services/course.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+  styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent {
-
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.form = this.formBuilder.group( {
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar
+  ) {
+    this.form = this.formBuilder.group({
       name: [null],
-      category: [null]
+      category: [null],
     });
   }
 
   onSubmit() {
-
+    this.service.save(this.form.value).subscribe(
+      (result) => console.log(result),
+      (error) => this.onError()
+    );
   }
 
-  onCancel() {
+  onCancel() {}
 
+  private onError() {
+    this.snackBar.open('Erro ao salvar curso', '', { duration: 5000 });
   }
 }
--- src/app/courses/services/course.service.ts
+++ src/app/courses/services/course.service.ts
@@ -1,5 +1,5 @@
+import { Course } from './../model/course';
 import { Injectable } from '@angular/core';
-import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
 import { first, tap } from 'rxjs';
 
@@ -18,5 +18,9 @@ export class CoursesService {
       first(),
       tap(courses => console.log(courses))
     );
+  }
+
+  save(record: Course) {
+    return this.httpClient.post<Course>(this.API, record);
   }
 }
