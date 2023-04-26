be1db385947b63595eca88378d635061fd182ef9
Guilherme <guilherme.manzano93@gmail.com>
2023-04-26 00:56:19 -0300

    add forms courses

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,9 +1,10 @@
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/course.service';
 import { Observable, catchError, of } from 'rxjs';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -13,12 +14,14 @@ import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/err
 export class CoursesComponent implements OnInit {
 
   courses$: Observable<Course[]>;
-  displayedColumns = ['name', 'category'];
+  displayedColumns = ['name', 'category', 'actions'];
 
   constructor(
     private coursesService: CoursesService,
-    private dialog: MatDialog
-    ) {
+    private dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
     this.courses$ = this.coursesService.list()
     .pipe(
       catchError(error => {
@@ -35,4 +38,8 @@ export class CoursesComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  onAdd() {
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
 }
