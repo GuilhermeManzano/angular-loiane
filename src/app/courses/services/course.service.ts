db9b67310630dcec19b35a2688a98cad4e7624e5
Guilherme <guilherme.manzano93@gmail.com>
2023-04-26 23:16:20 -0300

    refactor update courses

--- src/app/courses/services/course.service.ts
+++ src/app/courses/services/course.service.ts
@@ -4,19 +4,17 @@ import { HttpClient } from '@angular/common/http';
 import { first, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesService {
-
   private readonly API = '/api/courses';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   list() {
-    return this.httpClient.get<Course[]>(this.API)
-    .pipe(
+    return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
-      tap(courses => console.log(courses))
+      tap((courses) => console.log(courses))
     );
   }
 
@@ -25,6 +23,17 @@ export class CoursesService {
   }
 
   save(record: Partial<Course>) {
+    if(record._id) {
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>) {
     return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Course>) {
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record).pipe(first());
   }
 }
