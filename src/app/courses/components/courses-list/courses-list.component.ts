fa02bce3cd521d649e6592dcad00aea1d0bf23ac
Guilherme <guilherme.manzano93@gmail.com>
2023-04-26 22:26:33 -0300

    add findy by id

--- src/app/courses/components/courses-list/courses-list.component.ts
+++ src/app/courses/components/courses-list/courses-list.component.ts
@@ -1,15 +1,15 @@
+import { Course } from './../../model/course';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Course } from '../../model/course';
 
 @Component({
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
-  styleUrls: ['./courses-list.component.scss']
+  styleUrls: ['./courses-list.component.scss'],
 })
 export class CoursesListComponent {
-
-  @Input() courses: Course[] = []
+  @Input() courses: Course[] = [];
   @Output() add = new EventEmitter(false);
+  @Output() edit = new EventEmitter(false);
 
   readonly displayedColumns = ['name', 'category', 'actions'];
 
@@ -17,5 +17,9 @@ export class CoursesListComponent {
 
   onAdd() {
     this.add.emit(true);
+  }
+
+  onEdit(course: Course) {
+    this.edit.emit(course);
   }
 }
